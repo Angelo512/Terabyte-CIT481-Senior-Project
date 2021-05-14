d786ec791a653cdeeffb62bb5f054e8df322a554
Lynn <70084203+LynnParrish2@users.noreply.github.com>
2021-05-13 19:55:56 -0700

    Initial Project2 TF

---
+++ Project2/Terraform/terabyteEC2.tf
@@ -0,0 +1,45 @@
+resource "aws_spot_instance_request" "spot1" {
+  ami                    = "ami-02701bcdc5509e57b"
+  spot_price             = "0.016"
+  instance_type          = "t2.micro"
+  spot_type              = "one-time"
+  wait_for_fulfillment   = "true"
+  key_name               = "cit481_terabyte0"
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  subnet_id              = aws_subnet.private-subnet1.id
+
+  tags = {
+    Name = "terabyte-spot-private1"
+  }
+}
+
+resource "aws_spot_instance_request" "spot2" {
+  ami                    = "ami-02701bcdc5509e57b"
+  spot_price             = "0.016"
+  instance_type          = "t2.micro"
+  spot_type              = "one-time"
+  wait_for_fulfillment   = "true"
+  key_name               = "cit481_terabyte0"
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  subnet_id              = aws_subnet.private-subnet2.id
+
+  tags = {
+    Name = "terabyte-spot-private2"
+  }
+}
+
+resource "aws_spot_instance_request" "spot3" {
+  ami                    = "ami-02701bcdc5509e57b"
+  spot_price             = "0.016"
+  instance_type          = "t2.micro"
+  spot_type              = "one-time"
+  wait_for_fulfillment   = "true"
+  key_name               = "cit481_terabyte0"
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  subnet_id              = aws_subnet.private-subnet3.id
+
+  tags = {
+    Name = "terabyte-spot-private3"
+  }
+}
+
