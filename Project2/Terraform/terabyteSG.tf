ab08b820fb589047ef855ab09ab77c2d562f7db0
Lynn <70084203+LynnParrish2@users.noreply.github.com>
2021-05-15 13:51:43 -0700

    Add 443 SG

--- Project2/Terraform/terabyteSG.tf
+++ Project2/Terraform/terabyteSG.tf
@@ -11,6 +11,16 @@ resource "aws_security_group" "allow_http_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   ingress {
     description = "SSH from VPC"
     from_port   = 22
